0a23cdce8a177dd9f0c1c949de5fa5d88c11203b
tseker <146738908+tseker@users.noreply.github.com>
2024-06-26 15:51:07 +0200

    Delete github directory

--- github/Zhihang_Liang_Tuba_ŞekerPS3/exercise6/focn.m
+++
@@ -1,8 +0,0 @@
-function [foc] = focn(sj,n, wage, r0)
-
-
-global beta mu A delta alpha s b N prob kk kap v dist phi B n_pol c_pol Ntarget
-
-    foc = B*n.^(phi)-(wage*sj*((1+r0)*kap' + wage*sj*n - kap).^(-mu));
-
-end
